f9f96a43e3898845413d22de5ca9eecde13bcd04
MD SABBIR KHAN <64284482+md-sabbir-khan@users.noreply.github.com>
2020-10-01 14:50:39 -0700

    Create mail.py

---
+++ mail.py
@@ -0,0 +1,30 @@
+import smtplib
+
+SERVER = ('smtp.gmail.com')
+
+FROM = "sender@gmail.com"
+password="sender-password"
+
+TO = ["reciver-mail"] # must be a list
+
+SUBJECT = "OPERATION TEAM"
+
+TEXT = (" DEPLOYMENT IS Failed")
+
+# Prepare actual message
+
+message = """\
+From: %s
+To: %s
+Subject: %s
+
+%s
+""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+
+# Send the mail
+
+server = smtplib.SMTP(SERVER)
+server.starttls() 
+server.login(FROM, password) 
+server.sendmail(FROM, TO, message)
+server.quit()
